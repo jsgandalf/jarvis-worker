237d17a631595e3638096ad4d7686c02d11aa338
Sean Thomas <sean.alan.thomas@gmail.com>
2021-05-07 10:04:34 -0700

    todo to remove bodyparser

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+// TODO remove bodyParser it is deprecated
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
