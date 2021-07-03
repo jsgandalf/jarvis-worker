a2dd4633e00f49bb918dceb68ab3e4fdde5daab8
Sean Thomas <sean.alan.thomas@gmail.com>
2021-07-03 15:44:02 -0700

    adding support for multiple signals

--- actions/signal.ts
+++ actions/signal.ts
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 import threeCommasAPI from '3commas-api-node';
+import { CLIENT_RENEG_WINDOW } from 'node:tls';
 
 const startNewDeal = (botId, reqPair, api) => {
     const payload = {
@@ -13,42 +14,52 @@ const startNewDeal = (botId, reqPair, api) => {
     return api.botStartNewDeal(payload)
 }
 
-
-export default (req, res) => {
-    
-    // check if there is a payload
-    if(!req.body) return res.status(400).send('provide a body');
-
-    const reqToken = req.body.token;
-    const reqBotId = req.body.botId;
-    const reqPair = req.body.pair;
-    const reqCyberBull = req.body.cyberbull;
+const processDeal = body => {
+    const reqToken = body.token;
+    const reqBotId = body.botId;
+    const reqPair = body.pair;
+    const reqKey = body.key;
+    const reqSecret = body.secret;
     // check for a passed token
-    if(!reqToken) return res.status(400).send('provide a token');
+    if (!reqToken) throw new Error('provide a token');
 
     // validate token
-    if (process.env.token !== reqToken) return res.status(403);
+    if (process.env.token !== reqToken) throw new Error('unauthorized');
     const creds = {
         apiKey: process.env.THREE_COMMAS_API_KEY,
         apiSecret: process.env.THREE_COMMAS_API_SECRET,
     };
-    if (reqCyberBull) {
-        creds.apiKey = process.env.THREE_COMMAS_API_KEY_CYBERBULL;
-        creds.apiSecret = process.env.THREE_COMMAS_API_SECRET_CYBERBULL;
+    if (reqKey) {
+        creds.apiKey = process.env[reqKey];
+        creds.apiSecret = process.env[reqSecret];
     }
     const api = new threeCommasAPI(creds);
     const promises = Array.isArray(reqBotId) ? reqBotId.map(id => startNewDeal(id, reqPair, api)) : [startNewDeal(reqBotId, reqPair, api)];
-    Promise.all(promises).then((data) => {
+    return Promise.all(promises);
+}
+
+
+export default async (req, res) => {
+    
+    // check if there is a payload
+    if(!req.body) return res.status(400).send('provide a body');
+    try {
+        let promises;
+        if (Array.isArray(req.body) && req.body.length > 0) {
+            promises = req.body.map((payload => processDeal(payload)));
+        } else {
+            promises.push(processDeal(req.body));
+        }
+        const data = await Promise.all(promises);
         console.log(data);
         //@ts-ignore
-        if(data.error && data.error !== '') {
+        if (data.error && data.error !== '') {
             //@ts-ignore
-            throw data.error;
+            throw new Error(data.error);
         }
-        res.status(200).send(data);
-    }).catch((error) => {
-        console.error(error);
-        return res.send(error);
-    });
-    return res.status(501);
+        return res.status(200).send(data);
+    } catch (e) {
+        if (e === 'unauthorized') return res.status(403);
+        return res.status(400).send(e.message);
+    }
 }
